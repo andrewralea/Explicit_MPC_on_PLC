50ab6b5874966d68512bf1cdfe1af066ae9c6d42
andrewralea <mralea99@gmail.com>
2021-05-16 16:33:39 -0400

    Reformat PWA

--- Quad-Tank/MATLAB_Files/PWA_Format.m
+++ Quad-Tank/MATLAB_Files/PWA_Format.m
@@ -6,12 +6,14 @@ temp_array = size(mpcobjExplicitSimplified.PiecewiseAffineSolution);
 num_regions = temp_array(2);
 
 % Deal with F Matrix
-fprintf(fid_o, 'PWA_F: ARRAY [0..%d] OF REAL := [', num_regions - 1);
+temp_array = size(mpcobjExplicitSimplified.PiecewiseAffineSolution(1).F);
+num_elements = temp_array(2);
+fprintf(fid_o, 'PWA_F: ARRAY [1..%d] OF ARRAY [1..%d] OF REAL := [', num_regions, num_elements);
 for i = 1:num_regions
     temp_array = size(mpcobjExplicitSimplified.PiecewiseAffineSolution(i).F);
     num_elements = temp_array(2);
     num_rows = temp_array(1);
-    fprintf(fid_o, '(');
+    fprintf(fid_o, '[');
     for j = 1:num_rows
         for k = 1:num_elements
             if (k == num_elements && j == num_rows)
@@ -22,81 +24,10 @@ for i = 1:num_regions
         end
     end
     if (i ~= num_regions)
-        fprintf(fid_o, '),\n\t');
+        fprintf(fid_o, '],\n\t');
     else
-        fprintf(fid_o, ')');
+        fprintf(fid_o, ']');
     end
 end
-fprintf(fid_o, ']\n');
+fprintf(fid_o, '];\n');
 
-% Deal with G Matrix
-fprintf(fid_o, 'PWA_G: ARRAY [0..%d] OF REAL := [', num_regions - 1);
-for i = 1:num_regions
-    temp_array = size(mpcobjExplicitSimplified.PiecewiseAffineSolution(i).G);
-    num_elements = temp_array(2);
-    num_rows = temp_array(1);
-    fprintf(fid_o, '(');
-    for j = 1:num_rows
-        for k = 1:num_elements
-            if (k == num_elements && j == num_rows)
-                fprintf(fid_o, '%1.6f', mpcobjExplicitSimplified.PiecewiseAffineSolution(i).G(j, k));
-            else
-                fprintf(fid_o, '%1.6f,', mpcobjExplicitSimplified.PiecewiseAffineSolution(i).G(j, k));
-            end
-        end
-    end
-    if (i ~= num_regions)
-        fprintf(fid_o, '),\n\t');
-    else
-        fprintf(fid_o, ')');
-    end
-end
-fprintf(fid_o, ']\n');
-
-% Deal with H Matrix
-fprintf(fid_o, 'PWA_H: ARRAY [0..%d] OF REAL := [', num_regions - 1);
-for i = 1:num_regions
-    temp_array = size(mpcobjExplicitSimplified.PiecewiseAffineSolution(i).H);
-    num_elements = temp_array(2);
-    num_rows = temp_array(1);
-    fprintf(fid_o, '(');
-    for j = 1:num_rows
-        for k = 1:num_elements
-            if (k == num_elements && j == num_rows)
-                fprintf(fid_o, '%1.6f', mpcobjExplicitSimplified.PiecewiseAffineSolution(i).H(j, k));
-            else
-                fprintf(fid_o, '%1.6f,', mpcobjExplicitSimplified.PiecewiseAffineSolution(i).H(j, k));
-            end
-        end
-    end
-    if (i ~= num_regions)
-        fprintf(fid_o, '),\n\t');
-    else
-        fprintf(fid_o, ')');
-    end
-end
-fprintf(fid_o, ']\n');
-
-% Deal with K Matrix
-fprintf(fid_o, 'PWA_K: ARRAY [0..%d] OF REAL := [', num_regions - 1);
-for i = 1:num_regions
-    temp_array = size(mpcobjExplicitSimplified.PiecewiseAffineSolution(i).K);
-    num_elements = temp_array(2);
-    num_rows = temp_array(1);
-    fprintf(fid_o, '(');
-    for j = 1:num_rows
-        for k = 1:num_elements
-            if (k == num_elements && j == num_rows)
-                fprintf(fid_o, '%1.6f', mpcobjExplicitSimplified.PiecewiseAffineSolution(i).K(j, k));
-            else
-                fprintf(fid_o, '%1.6f,', mpcobjExplicitSimplified.PiecewiseAffineSolution(i).K(j, k));
-            end
-        end
-    end
-    if (i ~= num_regions)
-        fprintf(fid_o, '),\n\t');
-    else
-        fprintf(fid_o, ')');
-    end
-end
-fprintf(fid_o, ']\n');
